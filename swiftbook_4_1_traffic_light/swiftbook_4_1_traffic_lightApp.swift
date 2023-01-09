f419383b04a585ccea4ae408d9ec64503c7114a5
Gennady Dobretsov <dobrets@gmail.com>
2023-01-10 00:46:50 +0600

    Initial Commit

---
+++ swiftbook_4_1_traffic_light/swiftbook_4_1_traffic_lightApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftbook_4_1_traffic_lightApp.swift
+//  swiftbook_4_1_traffic_light
+//
+//  Created by dobrets on 10.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftbook_4_1_traffic_lightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
